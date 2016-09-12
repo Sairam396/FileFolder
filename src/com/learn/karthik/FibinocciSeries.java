73ccd00a731352f6110bb75dd09c8966d8227a7c
Karthik151 <karthik.varanasi51@gmail.com>
2016-09-12 18:52:57 -0500

    Master program has been changed

--- src/com/learn/karthik/FibinocciSeries.java
+++ src/com/learn/karthik/FibinocciSeries.java
@@ -1,6 +1,29 @@
 package com.learn.karthik;
 
+import java.util.Scanner;
+
 public class FibinocciSeries {
 	
-
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	
+	System.out.println("How many numbers woud you want in the fibinocci series");
+	int n=scan.nextInt();
+	
+	int a,b,c;
+	a=1;
+	b=2;
+	
+	int flag=1;
+	while(flag<=n){
+		System.out.println(a+",");
+		c=a+b;
+		a=b;
+		b=c;
+		flag++;
+		
+	}	
+	
 }
+}
+																																																																																																																																																																																																																																																																																																																																																																																																																																		
